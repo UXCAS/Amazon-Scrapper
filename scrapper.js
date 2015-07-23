d25a8dd3ba8d2b8be9165933fa90137321c1f96f
abhishekdewan101 <ronald.abhishek@gmail.com>
2015-07-23 11:11:25 -0700

    updated to have file type control for the user

--- scrapper.js
+++ scrapper.js
@@ -1,3 +1,4 @@
+
 var request = require('request'),
     cheerio = require('cheerio');
     fs = require('fs');
@@ -6,6 +7,7 @@ var request = require('request'),
     var ASIN;
     var PRODUCT_NAME;
     var MAX_NO_PAGES = 25;
+    var FILE_TYPE = "csv";
 
 
 
@@ -17,6 +19,9 @@ var request = require('request'),
 
     MAX_NO_PAGES = readlineSync.question('How many pages do you want to scrape?\n');
 
+     FILE_TYPE = readlineSync.question('What kind of file would you like? (txt or csv)\n');
+
+    if(FILE_TYPE === "csv"){
 
     fs.appendFile(PRODUCT_NAME+'review.csv', 'Title,Author,Stars,Review', function (err) {
        if (err) throw err;
@@ -26,7 +31,7 @@ var request = require('request'),
    fs.appendFile(PRODUCT_NAME+'review.csv','\n', function (err) {
       if (err) throw err;
   });
-  
+
     var percentagecomplete = 0;
      for(var i=1;i<=MAX_NO_PAGES;i++){
           console.log("Scraping Page Number: " + i);
@@ -55,4 +60,32 @@ var request = require('request'),
           percentagecomplete ++;
           console.log("Writing a very large file. Percentage Complete = "+ parseInt(((percentagecomplete/MAX_NO_PAGES) *100))+"%");
         });
-}
+    }
+  }else{
+    var percentagecomplete = 0;
+     for(var i=1;i<=MAX_NO_PAGES;i++){
+          console.log("Scraping Page Number: " + i);
+          request('http://www.amazon.com/Apple-iPhone-Silver-16-Unlocked/product-reviews/'+ASIN+'/ref=cm_cr_pr_btm_link_2?ie=UTF8&showViewpoints=2&sortBy=recent&reviewerType=all_reviews&formatType=all_formats&filterByStar=all_stars&pageNumber='+i, function(error, response, body) {
+            // Hand the HTML response off to Cheerio and assign that to
+            //  a local $ variable to provide familiar jQuery syntax.
+            var $ = cheerio.load(body);
+
+            // Exactly the same code that we used in the browser before:
+            // console.log($('#cm_cr-review_list'));
+            $('#cm_cr-review_list').children('div').each(function(){
+                // console.log($(this).find('.a-icon-alt').text());
+                var starRating = $(this).find('.a-icon-alt').text();
+                var title = $(this).find('.review-title').text();
+                var author = $(this).find('.author').text();
+                var reviewText = $(this).find('.review-text').text().trim();
+                if(reviewText != ""){
+                  fs.appendFile(PRODUCT_NAME+'review.txt',reviewText, function (err) {
+                     if (err) throw err;
+                 });
+              }
+          });
+          percentagecomplete ++;
+          console.log("Writing a very large file. Percentage Complete = "+ parseInt(((percentagecomplete/MAX_NO_PAGES) *100))+"%");
+        });
+    }
+  }
